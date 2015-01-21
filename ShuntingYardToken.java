769525a54a0b6d6e6afcb2a36ea5e27b27eac6a6
Daniel Baird <JamesDanielBaird@gmail.com>
2015-01-21 14:53:32 -0700

    Solved shunting token problem

--- ShuntingYardToken.java
+++ ShuntingYardToken.java
@@ -1,19 +1,22 @@
-
 public class ShuntingYardToken
 {
 	public enum Type
 	{
-	NUMBER
-	FUNCTION
-	FUNCTION_SEPARATOR
-	LEFT_PARENTHESIS
-	RIGHT_PARENTHESIS
+	NUMBER,
+	FUNCTION,
+	FUNCTION_SEPARATOR,
+	LEFT_PARENTHESIS,
+	RIGHT_PARENTHESIS,
 	OPERATOR	
 	}
-	
-	public ShuntingYardToken(??? typeEnum)
-	{
 
+	public String data;
+	public Type tokenType;
+
+	public ShuntingYardToken(Type tokenType,String data)
+	{
+		this.tokenType = tokenType;
+		this.data = data;
 	}
 
 }
